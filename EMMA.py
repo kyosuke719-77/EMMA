90192f6e51dc660018cc1954dddb69b53fb7947c
S19T329\minec <oor57kyosuke88uw@gmail.com>
2024-02-03 03:56:45 +0900

    first commit

---
+++ EMMA.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import tkinter
+from tkinter import ttk
+
+def main():
+    main_window = tkinter.Tk()
+    main_window.attributes('-topmost', False)
+    main_window.title('EMMA')
+    main_window.geometry("600x200")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+

94f6a5b70f66ae2fc3c84dafb7e700b7c9d7ff9e
kyosuke <oor57kyosuke88uw@gmail.com>
2024-02-09 19:35:11 +0900

    work from univ

--- EMMA.py
+++ EMMA.py
@@ -24,6 +24,25 @@ def isOk(value):
     return False
 
 
+def execute_emma():
+    file_path = file_select.get()
+    file_name = result_name.get()
+    minsup = minsup_value.get()
+    check = bool_val.get()
+
+    df = pd.read_csv(file_path)
+    data_size = df.to_numpy().shape
+
+    #入力データが1次元配列であるとき
+    if data_size[0] == 0:
+        data_size = list(data_size)
+        data_size[0] = 1
+    print(tuple(data_size))
+
+    #全データを1系列として
+
+
+
 def emma(data, data_size, minsup):
     episodes = []
     global compositte_episode
@@ -79,47 +98,49 @@ if __name__ == '__main__':
     main_window = tkinter.Tk()
     main_window.attributes('-topmost', False)
     main_window.title('EMMA')
-    main_window.geometry("600x200")
+    main_window.geometry("490x180")
 
     main_frm=ttk.Frame(main_window)
-    main_frm.grid(column=0, row=0, sticky=tkinter.NSEW, padx=5, pady=10)
+    # main_frm.grid(column=0, row=0, sticky=tkinter.NSEW, padx=5, pady=10)
     tcl_isOK = main_window.register(isOk)
 
     #ファイル選択ラベル
-    file_label = tkinter.Label(main_frm, text='ファイル選択')
-    file_label.grid(column=0, row=0, pady=10)
+    file_label = tkinter.Label(text='ファイル選択')
+    file_label.place(x=13, y=25)
 
     #ファイル選択のウィジェット変数
     file_select = tkinter.StringVar()
-    file_entry = ttk.Entry(main_frm, textvariable=file_select, width=50)
-    file_entry.grid(column=1, row=0, pady=10)
+    file_entry = ttk.Entry(textvariable=file_select, width=50)
+    file_entry.place(x=80, y=25)
 
     #ファイル選択ボタン
-    file_button = ttk.Button(main_frm, text='参照', command=get_filepath)
-    file_button.grid(column=2, row=0, pady=10)
+    file_button = ttk.Button(text='参照', command=get_filepath)
+    file_button.place(x=390, y=23)
 
     #出力ファイルラベル
-    result_label = ttk.Label(main_frm, text='ファイル名')
-    result_label.grid(column=0, row=1, pady=10)
+    result_label = ttk.Label(text='ファイル名')
+    result_label.place(x=19, y=55)
 
     #出力ファイル名入力欄
     result_name = ttk.Entry(width=50)
-    result_name.grid(column=0, row=1)
+    result_name.place(x=80, y=55)
 
     #minsup入力欄
-    minsup_labe = ttk.Label(main_frm, text='minspu:')
+    minsup_labe = ttk.Label(text='minsup')
+    minsup_labe.place(x=22, y=85)
     minsup_value = ttk.Entry(validate='key', validatecommand=(tcl_isOK, '%S'), textvariable=tkinter.StringVar())
-    minsup_value.grid(column=0, row=4)
+    minsup_value.place(x=80, y=85)
 
     #チェックボックス
     bool_val = tkinter.BooleanVar()
     bool_val.set(False)
     check_box = ttk.Checkbutton(text='全データを1系列に変換する', variable=bool_val)
-    check_box.grid(column=0, row=2, pady=10)
+    check_box.place(x=20, y=115)
 
     #実行ボタン
-    run_button = ttk.Button(main_frm, text='実行')
-    run_button.grid(column=1, row=4, pady=10)
+    run_button = ttk.Button(text='実行', command=execute_emma)
+    run_button.place(x=200, y=145)
+
     main_window.mainloop()
 
 
